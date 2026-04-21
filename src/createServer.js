8334eacf568e76e47aa90b9f814b0824fd81608a
Polianskyi Dmytro <dimdemon1@gmail.com>
2026-04-21 10:25:16 +0300

    add solution for fixes

--- src/createServer.js
+++ src/createServer.js
@@ -1,8 +1,64 @@
 'use strict';
 
+const http = require('http');
+const fs = require('fs');
+
 function createServer() {
   /* Write your code here */
   // Return instance of http.Server class
+  return http.createServer((req, res) => {
+    const filePath = req.url.pathname;
+
+    if (filePath.includes('../')) {
+      res.statusCode = 400;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end('path should not contain ..');
+
+      return;
+    } else if (filePath.includes('//')) {
+      res.statusCode = 404;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end('Invalid path');
+
+      return;
+    } else if (filePath === '/file') {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/plain');
+
+      res.end(
+        'path to file should start with /file/ and include the file name',
+      );
+
+      return;
+    } else if (!filePath.startsWith('/file/')) {
+      res.statusCode = 404;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end('path to file should start with /file/');
+
+      return;
+    }
+
+    const relativePath = filePath.slice('/file/'.length);
+    let fileData;
+
+    try {
+      fileData = fs.readFileSync(`./public/${relativePath}`);
+    } catch (error) {
+      res.statusCode = 404;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end(error.message);
+
+      return;
+    }
+    res.statusCode = 200;
+
+    if (filePath.endsWith('.html')) {
+      res.setHeader('Content-Type', 'text/html');
+    } else if (filePath.endsWith('.css')) {
+      res.setHeader('Content-Type', 'text/css');
+    }
+    res.end(fileData);
+  });
 }
 
 module.exports = {
